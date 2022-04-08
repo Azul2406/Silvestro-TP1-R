29b885c4beef7bd2369968b25d3f24ba615adf48
Azul2406 <silvestromariaazul@gmail.com>
2022-04-07 21:09:49 -0300

    Creación de clases y definición de categorias.

---
+++ Circulo RH negativo 1/Circulo RH negativo 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circulo_RH_negativo_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
